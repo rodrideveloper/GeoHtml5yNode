e1c7f55bd3c469b4fe7f603ef95c258e79bdb7fb
iLChopper <rodrigo.rodriguez@live.com.ar>
2014-09-12 11:02:38 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express=require('express');
+var app=express();
+
+app.use(express.static(__dirname + '/'));
+app.get('/',inicio);
+
+function inicio (req,res) {
+	res.render('index.html');
+}
+
+app.listen(3000);
